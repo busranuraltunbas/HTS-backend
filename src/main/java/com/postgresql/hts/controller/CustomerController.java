ff4e20da5a78e4f0dca6e6271adab42ab85dbdbc
busranuraltunbas <altunbasbusranur@gmail.com>
2024-10-22 01:02:18 +0300

    create and get customer

--- src/main/java/com/postgresql/hts/controller/CustomerController.java
+++ src/main/java/com/postgresql/hts/controller/CustomerController.java
@@ -3,6 +3,8 @@ package com.postgresql.hts.controller;
 import com.postgresql.hts.model.Customer;
 import com.postgresql.hts.repository.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
@@ -16,6 +18,7 @@ public class CustomerController{
     @Autowired
     CustomerRepo customerRepo;
 
+    //eski
     @PostMapping("/addCustomer")
     public void addCustomer(@RequestBody Customer customer){
         customer.setState(true);
@@ -34,9 +37,26 @@ public class CustomerController{
         return customerRepo.findAll();
     }
 
+    //build get customer by id REST API
     @GetMapping("/customer/{id}")
-    public Optional<Customer> getCustomerById(@PathVariable(value = "id") Long id){
-        return customerRepo.findById(id);
+    public ResponseEntity<Customer> getCustomerById(@PathVariable Long id){
+        Customer customer = customerRepo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer not exist with id" + id));
+        return ResponseEntity.ok(customer);
+    }
+
+    //build update customer REST API
+    @PutMapping("/customer/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Long id, @RequestBody Customer customerDetails){
+        Customer updateCustomer =  customerRepo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer not exist with id" + id));
+        updateCustomer.setFirstName(customerDetails.getFirstName());
+        updateCustomer.setLastName(customerDetails.getLastName());
+        updateCustomer.setPhone_number(customerDetails.getPhone_number());
+        updateCustomer.setAddress(customerDetails.getAddress());
+
+        customerRepo.save(updateCustomer);
+        return ResponseEntity.ok(updateCustomer);
     }
 
 }
